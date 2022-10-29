d35b16a59e921bc4ff3047e208c2eaf9cd780d67
Anett Pärismaa <anett@DESKTOP-F9I3U0D.localdomain>
2022-10-29 15:26:11 +0300

    tasks 1-3 done, task 4 started

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # WAD_Homework1
+
+## URI
+* https://api.npoint.io/b2488b205f84c767cd84
+* https://api.npoint.io/b2488b205f84c767cd84
+
+
+## Validating JSON
+* https://jsonlint.com/
+* https://www.npoint.io/docs/b2488b205f84c767cd84
---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+fetch('https://api.npoint.io/b2488b205f84c767cd84')
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+
+            // your code will be here
+                      })
