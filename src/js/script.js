c84e92821a9da7dfba7dfb3dd0a4c710691581e5
Greete Kelli Aava <greete.kelli.aava@ut.ee>
2022-10-31 21:50:20 +0200

    task4 completed

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,55 @@
+//https://www.javascripttutorial.net/javascript-fetch-api/
+/**
+ * function that fetches posts from endpoint
+ * @returns 
+ */
+async function fetchPosts(){
+    try {
+        let promise = await fetch('https://api.npoint.io/b2488b205f84c767cd84');
+        //let promise = await fetch('posts.json');
+        if (!promise.ok) {
+            throw new Error('Failed to fetch posts: ${promise.status}');
+        }
+        return await promise.json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+async function listsPosts() {
+    let data = await fetchPosts();
+    let html = '';
+    data.Posts.forEach(post => {
+        let htmlSegment = `<div class="article">
+            <img src="${post.img}"/>
+            <h4>${post.date}</h4>
+            <p>${post.body}</p>
+            </div>`;
+        html += htmlSegment;
+    })
+    const container = document.getElementById('posts');
+    console.log(container)
+    container.innerHTML = html;
+    //const element = document.getElementById("div1");
+    //element.appendChild(para);
+}
+
+listsPosts();
+
+/** 
 fetch('https://api.npoint.io/b2488b205f84c767cd84')
         .then((response) => response.json())
         .then(json => {
             console.log(json);
-
-            // your code will be here
-                      })
+            var posts = JSON.parse(JSON.stringify(json));
+            console.log(posts);
+            posts.forEach(post => {
+                console.log(post)
+            })
+            let htmlSegment = `<div class="article">
+                <img src="${post.img}"/>
+                <h4>${post.date}</h4>
+                <p>${post.body}</p>
+                </div>`;
+                html += htmlSegment;
+            })*/
